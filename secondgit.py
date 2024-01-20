6b5528ec8268a6a302eb3e54acd1d6048d399750
Ansaf Khan <ansafkhan2000@gmail.com>
2024-01-20 16:28:11 +0530

    start

---
+++ secondgit.py
@@ -0,0 +1,22 @@
+# #print ("again")
+# a =100
+# # b = 21
+
+# # print (a//b)
+# a+=10
+# print (a)
+# a/=5
+# print (a)
+# a-=10
+# print (a)
+# a*=2
+# print (a)
+# a%=3
+# print (a)
+b = 3
+c = 2
+# print (b**c)
+print (b<c)
+print (b>c)
+print (b==c)
+print (b!=c)
