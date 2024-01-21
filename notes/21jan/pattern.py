f80fca30dd6c1e7016fef6647051ec32cd432596
Ansaf Khan <ansafkhan2000@gmail.com>
2024-01-21 18:03:29 +0530

    pattern

---
+++ notes/21jan/pattern.py
@@ -0,0 +1,11 @@
+# https://i.pinimg.com/736x/07/0d/e6/070de6bea184aa69c8c6b093c4e1ae5e.jpg
+
+# for i in range (6):
+#     for j in range (i):
+#      print ( "*" , end ="")
+#     print ("")
+
+for i in range (5):
+    for j in range (i):
+     print ( "*" , end ="")
+    print ("")
--- notes/21jan/whileloop.py
+++ notes/21jan/whileloop.py
@@ -1,21 +1,23 @@
-def calculator():
-     oper1 = int(input ("please enter 1st number --> "))
-     oper2 = int(input ("please enter 2nd number --> "))
-     aa = input("select operator + , - , / , * --> ")
-     if aa == "+" :
-            print (oper1 + oper2)
-     elif aa == "-":
-            print (oper1 - oper2)
-     elif aa == "*":
-            print(oper1 * oper2)
-     elif aa == "/":
-            print (oper1 / oper2)
-     else : 
-            ("invalid command")
+# def calculator():
+#      oper1 = int(input ("please enter 1st number --> "))
+#      oper2 = int(input ("please enter 2nd number --> "))
+#      aa = input("select operator + , - , / , * --> ")
+#      if aa == "+" :
+#             print (oper1 + oper2)
+#      elif aa == "-":
+#             print (oper1 - oper2)
+#      elif aa == "*":
+#             print(oper1 * oper2)
+#      elif aa == "/":
+#             print (oper1 / oper2)
+#      else : 
+#             ("invalid command")
 
-while True :
-       calculator()
-       user_input = input(("do you want to stop Y/N -->  "))         
-       if user_input == "Y":
-          print("Command Stoped Succesfully")
-          break
+# while True :
+    #    calculator()
+    #    user_input = input(("do you want to stop Y/N -->  "))         
+    #    if user_input == "Y":
+    #       print("Command Stoped Succesfully")
+    #       break
+
+print (8-10*2/3)
