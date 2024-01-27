2f145d0d0d16ad8655ab5efc8704cd312d9fe976
Ansaf Khan <ansafkhan2000@gmail.com>
2024-01-27 17:53:05 +0530

    pattern

--- notes/21jan/pattern.py
+++ notes/21jan/pattern.py
@@ -5,7 +5,123 @@
 #      print ( "*" , end ="")
 #     print ("")
 
-for i in range (5):
-    for j in range (i):
-     print ( "*" , end ="")
-    print ("")
+# for i in range (5):
+#     for j in range (i):
+#      print ( "*" , end ="")
+#     print ("")
+
+# row = 5
+# for  i in range (1 , row +1):
+#     # for space
+#     for j in range (row-i):
+#        print (" " , end =" ")
+#        #for star
+#     for k in range (i):
+#             print ("*",end= " ")
+#     print (" ")
+
+# user = int(input ("please enter the row number: "))
+# row = user
+# for  i in range (1 , row +1):
+#     # for space
+#     for j in range (row-i):
+#        print (" " , end =" ")
+#        #for star
+#     for k in range (i):
+#             print ("*",end= " ")
+#     print (" ")
+
+# row = 5
+# for i in range (1,row +1):
+#     # for space
+#     for j in range (row -i):
+#         print (" ",end = "")
+#         # for star
+#     for k in range (i):
+#         print ("*",end= " ")
+#         #for next line
+#     print(" ")
+
+# row = 5
+# for i in range (1, row +1):
+#     for j in range (1, row +1):
+#         if i == 1 or i == 5 or j ==1 or j == 5 :
+#             print("*", end= " ")
+#         else :
+#             print (" ", end =" ")
+#     print (" ")
+
+
+# row = 5
+# for i in range (1, row +1):
+#     for j in range (1, row +1):
+#             print("*", end= " ")
+#     print (" ")
+
+# for i in range (6):
+#     for j in range (i):
+#      print ( "*" , end =" ")
+#     print ("*") 
+# for k in range (6):
+#     for l in range (i-k):
+#      print ( "$" , end =" ")
+#     print ("$") 
+
+
+# row = 5
+# for  i in range (1 , row +1):
+#     # for space
+#     for j in range (row-i):
+#        print (" " , end =" ")
+#        #for star
+#     for k in range (i):
+#             print ("*",end= " ")
+#     print ("*")
+# for  l in range (1 , row +1):
+#     # for space
+#     for m in range (l):
+#        print (" " , end =" ")
+#        #for star
+#     for o in range (i-l):
+#             print ("$",end= " ")
+#     print ("$")
+
+# row = 5
+# for i in range (1,row +1):
+#     # for space
+#     for j in range (row -i):
+#         print (" ",end = "")
+#         # for star
+#     for k in range (i):
+#         print ("*",end= " ")
+#         #for next line
+#     print("*")
+# for l in range (1,row +1):
+#     # for space
+#     for m in range (l):
+#         print (" ",end = "")
+#         # for star
+#     for n in range (i-l):
+#         print ("$",end= " ")
+#         #for next line
+#     print("$")
+row =5
+for l in range (row ):
+    # for space
+    for m in range (l):
+        print (" ",end = "")
+        # for star
+    for n in range (row -l):
+        print ("$",end= " ")
+        #for next line
+    print("$")
+for i in range (row +1):
+    # for space
+    for j in range (row -i):
+        print (" ",end = "")
+        # for star
+    for k in range (i):
+        print ("$",end= " ")
+        #for next line
+    print("$")
+
